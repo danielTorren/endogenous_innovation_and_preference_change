4694e6629e15c35533555718ffe5084e853ddbde
Daniel Torren Peraire <1621511@uab.cat>
2024-12-22 18:52:44 +0100

    again

--- package/calibration/NN_multi_round_calibration_multi_gen.py
+++ package/calibration/NN_multi_round_calibration_multi_gen.py
@@ -173,8 +173,7 @@ if __name__ == "__main__":
     parameters_list = [
         {"name": "a_innovativeness", "subdict": "parameters_social_network", "bounds": [0.05, 3]},
         {"name": "b_innovativeness", "subdict": "parameters_social_network", "bounds": [0.05, 3]},
-        {"name": "mu", "subdict": "parameters_vehicle_user", "bounds": [0.01, 2]},
-        {"name": "kappa", "subdict": "parameters_vehicle_user", "bounds": [5, 30]},
+        {"name": "kappa", "subdict": "parameters_vehicle_user", "bounds": [5, 25]},
         {"name": "SW_prob_rewire", "subdict": "parameters_social_network", "bounds": [0, 1]},
         {"name": "SW_K", "subdict": "parameters_social_network", "bounds": [20, 500]},
     ]
