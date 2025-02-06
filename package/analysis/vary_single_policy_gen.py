b967182a9663948d79a278dbfc3b48d37f0eecc5
Daniel Torren Peraire <1621511@uab.cat>
2025-02-06 23:18:12 +0100

    mroe runs

--- package/analysis/vary_single_policy_gen.py
+++ package/analysis/vary_single_policy_gen.py
@@ -147,7 +147,7 @@ def main(
 if __name__ == "__main__":
     main(
         BASE_PARAMS_LOAD="package/constants/base_params_vary_single_policy_gen.json",
-        repetitions=200,
+        repetitions=20,
         policy_list = [
             "Carbon_price",
             "Discriminatory_corporate_tax",
