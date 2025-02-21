ebad29a4915a97a6544bc66163c589e1f5016c88
Daniel Torren Peraire <1621511@uab.cat>
2025-02-21 10:27:47 +0100

    run NN longer

--- package/calibration/NN_multi_round_calibration_multi_gen.py
+++ package/calibration/NN_multi_round_calibration_multi_gen.py
@@ -179,8 +179,8 @@ if __name__ == "__main__":
     parameters_list = [
         {"name": "a_chi", "subdict": "parameters_social_network", "bounds": [0.7, 3]},
         {"name": "b_chi", "subdict": "parameters_social_network", "bounds": [0.7, 3]},
-        {"name": "proportion_zero_target", "subdict": "parameters_social_network", "bounds": [0.005, 0.05]},
-        {"name": "kappa", "subdict": "parameters_vehicle_user", "bounds": [1, 3]},
+        {"name": "proportion_zero_target", "subdict": "parameters_social_network", "bounds": [0.005, 0.03]},
+        {"name": "kappa", "subdict": "parameters_vehicle_user", "bounds": [1, 2]},
         #{"name": "alpha", "subdict": "parameters_vehicle_user", "bounds": [0.4, 0.6]},
     ]
     main(
@@ -188,5 +188,5 @@ if __name__ == "__main__":
         BASE_PARAMS_LOAD="package/constants/base_params_NN.json",
         OUTPUTS_LOAD_ROOT="package/calibration_data",
         OUTPUTS_LOAD_NAME="calibration_data_output",
-        num_simulations=32
+        num_simulations=128
     )
