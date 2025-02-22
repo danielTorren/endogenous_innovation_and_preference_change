5c79d7e0f696b52385ff1736a4220177f0e4c7be
Daniel Torren Peraire <1621511@uab.cat>
2025-02-23 00:32:47 +0100

    NN

--- package/calibration/NN_multi_round_calibration_multi_gen.py
+++ package/calibration/NN_multi_round_calibration_multi_gen.py
@@ -179,9 +179,9 @@ def main(
 
 if __name__ == "__main__":
     parameters_list = [
-        {"name": "a_chi", "subdict": "parameters_social_network", "bounds": [1, 4]},
-        {"name": "b_chi", "subdict": "parameters_social_network", "bounds": [1, 4]},
-        {"name": "proportion_zero_target", "subdict": "parameters_social_network", "bounds": [0.001, 0.03]},
+        {"name": "a_chi", "subdict": "parameters_social_network", "bounds": [1, 3]},
+        {"name": "b_chi", "subdict": "parameters_social_network", "bounds": [1, 3]},
+        {"name": "proportion_zero_target", "subdict": "parameters_social_network", "bounds": [0.005, 0.03]},
         #{"name": "kappa", "subdict": "parameters_vehicle_user", "bounds": [1, 2]},
         #{"name": "alpha", "subdict": "parameters_vehicle_user", "bounds": [0.4, 0.6]},
     ]
