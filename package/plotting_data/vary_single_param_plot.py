839c4169c9a0bf014433a0ddc813d653765f5997
Daniel Torren Peraire <1621511@uab.cat>
2025-02-24 22:10:34 +0100

    lambda var

--- package/plotting_data/vary_single_param_plot.py
+++ package/plotting_data/vary_single_param_plot.py
@@ -395,4 +395,4 @@ def main(fileName, dpi=600):
     plt.show()
 
 if __name__ == "__main__":
-    main("results/single_param_vary_21_54_49__24_02_2025")
+    main("results/single_param_vary_22_00_40__24_02_2025")
