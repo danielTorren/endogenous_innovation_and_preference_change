0bd5b768964c013b96eb1bd78631a6afdec7e49a
Daniel Torren Peraire <1621511@uab.cat>
2025-02-27 10:41:23 +0100

    run the varations

--- package/generating_data/vary_single_param_gen.py
+++ package/generating_data/vary_single_param_gen.py
@@ -63,5 +63,5 @@ def main(
 if __name__ == "__main__":
     results = main(
         BASE_PARAMS_LOAD="package/constants/base_params_vary_single_delta.json",
-        VARY_LOAD ="package/constants/vary_single_a_innov.json", #"package/constants/vary_single_delta.json"
+        VARY_LOAD ="package/constants/vary_single_lambda.json", #"package/constants/vary_single_delta.json"
         )
