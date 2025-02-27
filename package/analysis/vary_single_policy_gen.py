533be06d0eec57d4766fd13729374f0a9d103776
Daniel Torren Peraire <1621511@uab.cat>
2025-02-27 10:22:16 +0100

    run policy

--- package/analysis/vary_single_policy_gen.py
+++ package/analysis/vary_single_policy_gen.py
@@ -160,7 +160,7 @@ def main(
 if __name__ == "__main__":
     main(
         BASE_PARAMS_LOAD="package/constants/base_params_vary_single_policy_gen.json",
-        repetitions=50,
+        repetitions=10,
         policy_list = [
             "Carbon_price",
             "Discriminatory_corporate_tax",
