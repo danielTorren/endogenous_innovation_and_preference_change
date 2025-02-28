38baedc29fa91e584818b847ca1c5ae03f03db9b
Daniel Torren Peraire <1621511@uab.cat>
2025-02-28 13:03:31 +0100

    BO for 40

--- package/analysis/endogenous_policy_intensity_pair_gen.py
+++ package/analysis/endogenous_policy_intensity_pair_gen.py
@@ -122,13 +122,13 @@ if __name__ == "__main__":
         BASE_PARAMS_LOAD="package/constants/base_params_endogenous_policy_pair_gen.json",
         BOUNDS_LOAD="package/analysis/policy_bounds_vary_pair_policy_gen.json", 
         policy_list_all=[
-            "Carbon_price",
-            "Discriminatory_corporate_tax",
-            "Electricity_subsidy",
-            "Adoption_subsidy",
-            "Adoption_subsidy_used",
-            #"Production_subsidy",
-            #"Research_subsidy"
+            #"Carbon_price",
+            #"Discriminatory_corporate_tax",
+            #"Electricity_subsidy",
+            #"Adoption_subsidy",
+            #"Adoption_subsidy_used",
+            "Production_subsidy",
+            "Research_subsidy"
         ],
         policy_list_works=["Carbon_price"],
         target_ev_uptake=0.8,
