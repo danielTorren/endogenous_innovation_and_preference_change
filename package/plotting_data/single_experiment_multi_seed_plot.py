43ef2012d33e5465e62426ec9c41351fc32a39d4
Daniel Torren Peraire <1621511@uab.cat>
2025-03-18 12:30:24 +0100

    3000 with 4

--- package/plotting_data/single_experiment_multi_seed_plot.py
+++ package/plotting_data/single_experiment_multi_seed_plot.py
@@ -945,4 +945,4 @@ def main(fileName, dpi=300):
     plt.show()
 
 if __name__ == "__main__":
-    main("results/multi_seed_single_11_14_37__18_03_2025")
+    main("results/multi_seed_single_12_26_04__18_03_2025")
