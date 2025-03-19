eed7b1861dfee9300398ad05cf1ec47389de37b1
Daniel Torren Peraire <1621511@uab.cat>
2025-03-19 09:28:14 +0100

    test NN output

--- package/plotting_data/single_experiment_multi_seed_plot.py
+++ package/plotting_data/single_experiment_multi_seed_plot.py
@@ -953,4 +953,4 @@ def main(fileName, dpi=300):
     plt.show()
 
 if __name__ == "__main__":
-    main("results/multi_seed_single_18_32_57__18_03_2025")
+    main("results/multi_seed_single_09_07_40__19_03_2025")
