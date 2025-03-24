d7eaa75d493809998b51b6c5fdd61c5630222d77
Daniel Torren Peraire <1621511@uab.cat>
2025-03-24 12:09:23 +0100

    2

--- package/analysis/endogenous_policy_intensity_pair_gen.py
+++ package/analysis/endogenous_policy_intensity_pair_gen.py
@@ -117,15 +117,14 @@ def main(
         bounds_dict = json.load(f)
 
     policy_pairs = generate_unique_policy_pairs(policy_list_all, dont_work_list)
-    print("policy_pairs", policy_pairs)
+    
     #( 'Electricity_subsidy', 'Adoption_subsidy_used'),  
     policy_pairs = [
-        ( 'Adoption_subsidy_used','Adoption_subsidy'), 
-        #( 'Electricity_subsidy', 'Adoption_subsidy'),
-
+        #( 'Adoption_subsidy_used','Adoption_subsidy'), 
+        ( 'Electricity_subsidy', 'Adoption_subsidy'),
         #('Electricity_subsidy', 'Carbon_price' ) 
     ]
-
+    print("policy_pairs", policy_pairs)
     #print(policy_pairs[:5])
     #print(policy_pairs[5:8])
     #print(policy_pairs[8:])
