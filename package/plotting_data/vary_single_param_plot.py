2525928df5712ca49b3047ebf17d6015eeaa2142
Daniel Torren Peraire <1621511@uab.cat>
2025-03-26 16:39:47 +0100

    test

--- package/plotting_data/vary_single_param_plot.py
+++ package/plotting_data/vary_single_param_plot.py
@@ -500,4 +500,4 @@ def main(fileName, dpi=300):
     plt.show()
 
 if __name__ == "__main__":
-    main("results/single_param_vary_16_17_50__26_03_2025")
+    main("results/single_param_vary_16_24_46__26_03_2025")
