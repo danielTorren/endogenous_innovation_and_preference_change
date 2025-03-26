7ad5a63de03bb0bc5379cd8894fd9f5eda508311
Daniel Torren Peraire <1621511@uab.cat>
2025-03-26 23:17:17 +0100

    raise price

--- package/plotting_data/single_experiment_multi_seed_plot.py
+++ package/plotting_data/single_experiment_multi_seed_plot.py
@@ -1008,4 +1008,4 @@ def main(fileName, dpi=300):
     plt.show()
 
 if __name__ == "__main__":
-    main("results/multi_seed_single_17_45_24__26_03_2025")
+    main("results/multi_seed_single_17_54_02__26_03_2025")
