83e28b54f3b2c31074f93b078e994ddad7334b94
Daniel Torren Peraire <1621511@uab.cat>
2025-04-02 00:11:33 +0200

    run pairs and endog

--- package/analysis/endogenous_policy_intensity_pair_gen.py
+++ package/analysis/endogenous_policy_intensity_pair_gen.py
@@ -168,7 +168,7 @@ def main(
 
     policy_pairs = generate_all_policy_pairs(policy_list_all)
     print(policy_pairs)
-    quit()
+
     
     controller_files, base_params, file_name = set_up_calibration_runs(base_params,"endog_pair")
 
