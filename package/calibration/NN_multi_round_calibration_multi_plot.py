c899561bba5c4f6c4c4309b12c4b7f3ac1704581
Daniel <dtorrenp@hotmail.com>
2026-08-14 16:10:17 +0200

    lets do the EV cost adn the correaltion

--- package/calibration/NN_multi_round_calibration_multi_plot.py
+++ package/calibration/NN_multi_round_calibration_multi_plot.py
@@ -151,8 +151,11 @@ def main(fileName):
 
 if __name__ == "__main__":
     main(
-        fileName="results/sbi_single_seed_16_43_32__12_08_2026",
+        fileName="results/sbi_single_seed_13_04_51__14_08_2026",
     )
+    #sbi_single_seed_13_04_51__14_08_2026
+    #sbi_single_seed_12_42_40__14_08_2026
+    #NN_calibration_multi_12_34_29__14_08_2026
     #sbi_single_seed_16_43_32__12_08_2026
     #NN_calibration_multi_16_42_17__12_08_2026
     #sbi_single_seed_14_23_24__11_08_2026
