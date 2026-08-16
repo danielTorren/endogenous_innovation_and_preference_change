f15226c172f9d0bd49e0237f87aeee32c5eb9613
Daniel <dtorrenp@hotmail.com>
2026-08-16 18:52:35 +0200

    before messing with stuff sunday

--- package/calibration/NN_multi_round_calibration_multi_plot.py
+++ package/calibration/NN_multi_round_calibration_multi_plot.py
@@ -151,12 +151,15 @@ def main(fileName):
 
 if __name__ == "__main__":
     main(
-        fileName="results/NN_calibration_multi_13_02_48__14_08_2026",
+        fileName="results/NN_calibration_multi_12_34_29__14_08_2026",
     )
+    #sbi_single_seed_14_34_13__14_08_2026
+    #NN_calibration_multi_14_33_36__14_08_2026
     #NN_calibration_multi_13_02_48__14_08_2026
     #sbi_single_seed_13_04_51__14_08_2026
     #sbi_single_seed_12_42_40__14_08_2026
     #NN_calibration_multi_12_34_29__14_08_2026
+
     #sbi_single_seed_16_43_32__12_08_2026
     #NN_calibration_multi_16_42_17__12_08_2026
     #sbi_single_seed_14_23_24__11_08_2026
